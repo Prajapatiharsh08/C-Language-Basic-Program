e1fea8ccf837222a33f3645183781b3bc00d5182
Prajapatiharsh08 <159774216+Prajapatiharsh08@users.noreply.github.com>
2024-02-13 16:29:22 +0530

    Add files via upload

---
+++ Task1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	printf("Name : Prajapati Harsh\n");
+	
+	printf("Age : 18 Years\n");
+
+	printf("School : P.K Patel & U.D. Bhatt Higher Secondary School.");
+}
---
+++ Task2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+	printf("---------\n|       |\nR       |\nN       |\nW       |\n|       |\n---------\n");
+
+}
